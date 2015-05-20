8a0213b5b8a519cf6483f382e4fe6bfd28a0d3ca
Juho Vepsalainen <bebraw@gmail.com>
2015-05-20 21:06:18 +0300

    Update configuration to use dependency injection pattern
    Now Webpack plugins are passed here from core. Avoids some weird issues during
    development.

--- index.js
+++ index.js
@@ -1,58 +1,63 @@
 'use strict';
 var _ = require('lodash');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-var ReactHotLoaderMatches = /View.coffee|Pages\//;
 
 
 module.exports = {
-  common: {
-    resolve: {
-      extensions: ['.coffee']
-    }
+  common: function() {
+    return {
+      resolve: {
+        extensions: ['.coffee']
+      }
+    };
   },
-  build: {
-    module: {
-      loaders: [
-        {
-          test: /\.scss$/,
-          loader: ExtractTextPlugin.extract(
-            'style-loader',
-            'css-loader!autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}!sass-loader'),
-        },
-        {
-          test: /\.svg$/,
-          loader: 'raw-loader',
-        },
-        {
-          test: /\.coffee$/,
-          loader: 'coffee-loader'
-        }
-      ]
-    }
+  build: function(plugins) {
+    var ExtractTextPlugin = plugins.ExtractTextPlugin;
+
+    return {
+      module: {
+        loaders: [
+          {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract(
+              'style-loader',
+              'css-loader!autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}!sass-loader'),
+          },
+          {
+            test: /\.svg$/,
+            loader: 'raw-loader',
+          },
+          {
+            test: /\.coffee$/,
+            loader: 'coffee-loader'
+          }
+        ]
+      }
+    };
   },
-  development: {
-    module: {
-      loaders: [
-        {
-          test: /\.scss$/,
-          loaders: [
-            'style-loader',
-            'css-loader',
-            'autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}',
-            'sass-loader',
-          ],
-        },
-        {
-          test: /\.svg$/,
-          loader: 'raw-loader',
-        },
-        {
-          test: /\.coffee$/,
-          loader: 'react-hot!coffee-loader',
-        },
-      ]
-    }
+  development: function() {
+    return {
+      module: {
+        loaders: [
+          {
+            test: /\.scss$/,
+            loaders: [
+              'style-loader',
+              'css-loader',
+              'autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}',
+              'sass-loader',
+            ],
+          },
+          {
+            test: /\.svg$/,
+            loader: 'raw-loader',
+          },
+          {
+            test: /\.coffee$/,
+            loader: 'react-hot!coffee-loader',
+          },
+        ]
+      }
+    };
   },
   functions: {
     url: function(file, fileName) {
