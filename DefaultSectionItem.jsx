2b36661ac8d1f5b38aad3bd7093577f317aa7d9f
Juho Vepsalainen <bebraw@gmail.com>
2015-11-12 18:37:53 +0200

    Port DefaultSectionItem to jsx

---
+++ DefaultSectionItem.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Moment from './components/Moment';
+import Router from 'react-router';
+import config from 'config';
+
+module.exports = React.createClass({
+  displayName: 'DefaultSectionItem',
+  render() {
+    const page = this.props.page;
+
+    return (
+      <div className="post">
+        {page.headerImage ?
+          <div className="header-image" style={{
+            backgroundImage: `url(${page.headerImage})`
+          }} /> :
+          null
+        }
+
+        <h1 className="post__heading">{page.title}</h1>
+
+        <div className="post__content">
+          {page.isDraft ?
+            <span className="draft-text">Draft</span> :
+            null
+          }
+          <div dangerouslySetInnerHTML={{__html: page.content}} />
+        </div>
+        {page.date ?
+          <Moment className="post__moment" datetime={page.date} /> :
+          null
+        }
+        {page.author ?
+          <div className="post__author">{`Authored by ${page.author}`}</div> :
+          null
+        }
+      </div>
+    );
+  }
+});
