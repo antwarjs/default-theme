2fe6fdc78fa534da1861f4652e14f69cedb58e32
Juho Vepsalainen <bebraw@gmail.com>
2015-11-12 16:17:08 +0200

    Port Body to jsx
    Dropped analytics injection while at it. It's better to deal with that
    through a plugin.

---
+++ Body.jsx
@@ -0,0 +1,24 @@
+import React from  'react';
+
+import Nav from './Nav';
+
+import './scss/main.scss';
+
+import config from 'config';
+
+if(config.theme.customStyles) {
+  require('customStyles/' + config.theme.customStyles);
+}
+
+module.exports = React.createClass({
+  displayName: 'Body',
+  render() {
+    return (
+      <div>
+        <Nav />
+
+        <main role="main">{this.props.children}</main>
+      </div>
+    );
+  }
+});
