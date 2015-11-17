af98b3f120b370ce54856ca0a53901b9f814679a
Juho Vepsalainen <bebraw@gmail.com>
2015-11-17 13:24:46 +0200

    Expose layouts through package root

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 module.exports = {
-  build: function(plugins) {
-    var ExtractTextPlugin = plugins.ExtractTextPlugin;
+  layouts: require('./layouts'),
+  webpack: {
+    build: function(plugins) {
+      var ExtractTextPlugin = plugins.ExtractTextPlugin;
 
-    return {
-      module: {
-        loaders: [
-          {
-            test: /\.scss$/,
-            loader: ExtractTextPlugin.extract(
-              'style-loader',
-              'css-loader?minimize!autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}!sass-loader'),
-          }
-        ]
-      }
-    };
-  },
-  development: function() {
-    return {
-      module: {
-        loaders: [
-          {
-            test: /\.scss$/,
-            loaders: [
-              'style-loader',
-              'css-loader',
-              'autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}',
-              'sass-loader',
-            ],
-          }
-        ]
-      }
-    };
-  },
+      return {
+        module: {
+          loaders: [
+            {
+              test: /\.scss$/,
+              loader: ExtractTextPlugin.extract(
+                'style-loader',
+                'css-loader?minimize!autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}!sass-loader'),
+            }
+          ]
+        }
+      };
+    },
+    development: function() {
+      return {
+        module: {
+          loaders: [
+            {
+              test: /\.scss$/,
+              loaders: [
+                'style-loader',
+                'css-loader',
+                'autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}',
+                'sass-loader',
+              ],
+            }
+          ]
+        }
+      };
+    }
+  }
 };
---
+++ layouts/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  BlogPage: require('./BlogPage.jsx'),
+  DocsPage: require('./DocsPage.jsx'),
+  SectionIndex: require('.SectionIndex.jsx'),
+  SectionPage: require('./SectionPage.jsx')
+};
