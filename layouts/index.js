fe103634cd41fb2b4609d474a5259a1694518e05
Juho Vepsalainen <bebraw@gmail.com>
2015-11-18 14:04:44 +0200

    Drop redundant layouts

--- layouts/index.js
+++ layouts/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   BlogPage: require('./BlogPage.jsx'),
   DocsPage: require('./DocsPage.jsx'),
-  SectionIndex: require('./SectionIndex.jsx'),
-  SectionPage: require('./SectionPage.jsx')
+  SectionIndex: require('./SectionIndex.jsx')
 };
